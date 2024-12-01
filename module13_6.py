46619203b6f0a10a138db35f45165d02fc884569
sin013 <158771236+sin013@users.noreply.github.com>
2024-12-01 16:48:26 +0300

    Add files via upload

---
+++ module13_6.py
@@ -0,0 +1,85 @@
+from aiogram import Bot, Dispatcher, executor
+from aiogram.contrib.fsm_storage.memory import MemoryStorage
+from aiogram.dispatcher.filters.state import State, StatesGroup
+from aiogram.dispatcher import FSMContext
+from aiogram.types import ReplyKeyboardMarkup, KeyboardButton
+import asyncio
+
+api = "7354477285:AAFWpFyg81PFOkQHgisVwPUVtYCoDXd-rKs"
+bot = Bot(token=api)
+dp = Dispatcher(bot, storage=MemoryStorage())
+
+
+class UserState(StatesGroup):
+    age = State()
+    growth = State()
+    weight = State()
+
+
+kb = ReplyKeyboardMarkup()
+button1 = KeyboardButton(text='Рассчитать')
+button2 = KeyboardButton(text='Информация')
+kb.add(button1)
+kb.add(button2)
+
+
+@dp.message_handler(text='Привет')
+async def start(message):
+    await message.answer(f'Привет! Я бот помогающий Вашему здоровью.\n'
+                         f'Чтобы начать, нажмите "Рассчитать"', reply_markup=kb)
+
+
+@dp.message_handler(text='Рассчитать')
+async def set_age(message):
+    await message.answer('Введите свой возраст:')
+    await UserState.age.set()
+
+
+@dp.message_handler(state=UserState.age)
+async def set_growth(message, state):
+    await state.update_data(age=message.text)
+    await message.answer(f'Введите свой рост:')
+    await UserState.growth.set()
+
+
+@dp.message_handler(state=UserState.growth)
+async def set_weight(message, state):
+    await state.update_data(growth=message.text)
+    await message.answer(f'Введите свой вес:')
+    await UserState.weight.set()
+
+
+@dp.message_handler(state=UserState.weight)
+async def send_calories(message, state):
+    await state.update_data(weight=message.text)
+    data = await state.get_data()
+    age = int(data['age'])
+    growth = int(data['growth'])
+    weight = int(data['weight'])
+
+    calories_m = 10 * weight + 6.25 * growth - 5 * age + 5
+    calories_w = 10 * weight + 6.25 * growth - 5 * age - 161
+
+    global a
+    global g
+    global w
+    global c_m
+    global c_w
+    a = age
+    g = growth
+    w = weight
+    c_m = calories_m
+    c_w = calories_w
+    await message.answer('Расчёт произведён, посмотрите информацию')
+
+    @dp.message_handler(text='Информация')
+    async def inform(message):
+        await message.answer(f'Ваш возраст:  {a}\nВаш рост:      {g}\nВаш вес:          {w}\n'
+                             f'Ваша норма калорий: {c_m}, если вы мужчина\n    '
+                             f'                                      {c_w}, если вы женщина')
+
+    await state.finish()
+
+
+if __name__ == '__main__':
+    executor.start_polling(dp, skip_updates=True)
